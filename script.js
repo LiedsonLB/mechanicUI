1e05570376e1574bf84dbf464e3fe2c82b9ca686
Francisco Liedson Bonfim Barros <108279994+LiedsonLB@users.noreply.github.com>
2023-02-09 22:27:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,34 @@ var infos = document.querySelectorAll(".bi-info-circle-fill");
 var Message = document.getElementById("container-mensagem");
 var closeBtn = document.getElementById("close-modal");
 
-//abrindo modal
+// Função que alerta o ID da div clicada
 function infoClick() {
     Message.classList.remove("hide");
 }
 
-//clique nos botões de info
+// Associação de cada div à função
 for(var i=0; i<infos.length; i++) {
     infos[i].onclick = infoClick;
 }
-
-//fechando modal
 closeBtn.addEventListener("click", () => {
     Message.classList.add("hide");
+})
+
+//modal de confirmação
+var BackBtn = document.getElementById('back');
+var confirmBtn = document.getElementById('btn-confirm');
+var ConfirmModal = document.getElementById('confirm-modal');
+var confirma = document.getElementById('confirm-btn');
+var confirmService = document.getElementById('confirm-service')
+
+confirmBtn.addEventListener("click", () => {
+    ConfirmModal.classList.toggle("hide");
+})
+BackBtn.addEventListener("click", () => {
+    ConfirmModal.classList.toggle("hide");
+})
+confirma.addEventListener("click", () => {
+    ConfirmModal.classList.toggle("hide");
+    confirmBtn.classList.add("hide");
+    confirmService.classList.remove("hide");
 })
